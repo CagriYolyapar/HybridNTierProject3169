17904a155d7ba247a643ef9f48b328f168549373
cagri.yolyapar <cagri.yolyapar@gmail.com>
2023-11-21 11:59:49 +0300

    DAL katmanı tamamlandı

---
+++ Project.DAL/Repositories/Abstracts/ICategoryRepository.cs
@@ -0,0 +1,15 @@
+﻿using Project.ENTITIES.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.DAL.Repositories.Abstracts
+{
+    public interface ICategoryRepository:IRepository<Category>
+    {
+
+        void SpecialCategoryCreation(Category category,params string[] productName); // Kategori eklendiginde ismi belirtilen bir ürünle eklensin
+    }
+}
---
+++ Project.DAL/Repositories/Concretes/CategoryRepository.cs
@@ -0,0 +1,37 @@
+﻿using Project.DAL.ContextClasses;
+using Project.DAL.Repositories.Abstracts;
+using Project.ENTITIES.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.DAL.Repositories.Concretes
+{
+    public class CategoryRepository : BaseRepository<Category>, ICategoryRepository
+    {
+
+        public CategoryRepository(MyContext db) : base(db)
+        {
+
+        }
+        public void SpecialCategoryCreation(Category category,params string[] productName)
+        {
+            List<Product> products = new();
+            foreach (string product in productName) 
+            {
+                Product p = new()
+                {
+                    ProductName = product,
+                    UnitPrice = 10.3M
+                };
+                products.Add(p);
+            }
+
+            category.Products = products;
+
+            Add(category);
+        }
+    }
+}
